bbd587da5572bbe3830d11bd0cfe486ed4303de5
NoelKling <chaoticforbidden@gmail.com>
2020-10-06 17:00:19 -0500

    setting up photoslide, added faire image folder

--- javascript/main.js
+++ javascript/main.js
@@ -23,3 +23,31 @@ $(document).ready(function () {
     ],
   });
 });
+
+// Photo Slides
+
+let photoIndex = 1;
+
+showPhotos(photoIndex);
+
+function imageSlides(num) {
+  showPhotos((photoIndex += num));
+}
+
+function currentSlide(num) {
+  showPhotos((photoIndex = num));
+}
+
+function showPhotos(num) {
+  let slides = document.getElementsByClassName("photo-slides");
+  if (num > slides.length) {
+    photoIndex = 1;
+  }
+  if (num < 1) {
+    photoIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[photoIndex - 1].style.display = "block";
+}

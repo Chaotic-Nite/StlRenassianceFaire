4cd6d249df98dab65a2573cb7b3516b80077ac7f
NoelKling <chaoticforbidden@gmail.com>
2020-10-30 00:36:37 -0500

    Created dropdown menu in cast

--- javascript/cast.js
+++ javascript/cast.js
@@ -1,125 +1,166 @@
 const cast = {
-  court: [
+  Court: [
     "The royalty and noblity at the faire grounds. They perform they're noble duties whilst running amok in the crowds.",
     {
-      realName: "Joseph Fieger",
+      realName: "Joseph Fieger Sr",
       role: "Thomas Wolsey",
       position: "Cardinal of England",
-      image: "",
+      image: "/images/cast/joseph-fieger-sr.jpg",
     },
     {
       realName: "David Edwards",
       role: "King Henry VIII",
       position:
         "By the Grace of God, King of England and France, Defender of the Faith and Lord of Ireland",
-      image: "",
+      image: "/images/cast/david-edwards.jpg",
     },
     {
       realName: "Ted Jander",
       role: "King Francois",
       position: "King of France, Duke of Brittany",
-      image: "",
+      image: "/images/cast/ted-jander.jpg",
     },
     {
       realName: "Kathleen Mitchell",
       role: "Queen Katherine of Aragon",
       position: "Queen Consort of England, Princess of Spain",
-      image: "",
+      image: "/images/cast/kathleen-mitchell.jpg",
     },
     {
       realName: "Lanya da Silva",
       role: "Queen Claude",
       position: "Queen de France, Duchess of Britanny",
-      image: "",
+      image: "/images/cast/lanya-da-silva.jpg",
     }, //Continued positions: Holy Roman Empress, Empress of the Universe, Queen of England, Spain and EVERYTHING, Duchess of Milan
     {
       realName: "Trenton Brookesher",
       role: "Prince Henri",
       position: "Dauphin of France, Dauphin of Viennois",
-      image: "",
+      image: "/images/cast/trenton-brookesher.jpg",
     },
     {
       realName: "Jacqueline Brown",
       role: "Princess Mary",
       position: "Crown Princess of England",
-      image: "",
+      image: "/images/cast/jacqueline-brown.jpg",
     },
     {
       realName: "Kat Whalen",
       role: "Princess Katherine",
       position:
         "Princess of Portugal and Spain, ArchDuchess of Austria, Duchess of Braganza",
-      image: "",
+      image: "/images/cast/kathleen-whalen.jpg",
     },
-    { realName: "Sheana Godwin", role: "", position: "Duchess", image: "" },
+    {
+      realName: "Sheana Godwin",
+      role: "",
+      position: "Duchess",
+      image: "/images/cast/sheana-godwin.jpg",
+    },
     {
       realName: "Kristen Haynes",
       role: "Anne Boleyn",
       position: "Lady in Waiting",
-      image: "",
+      image: "/images/cast/kristen-haynes.jpg",
     },
     {
       realName: "Amber Fern",
       role: "Jane Seymour",
       position: "Lady in Waiting",
-      image: "",
+      image: "/images/cast/amber-fern.jpg",
+    },
+    {
+      realName: "Zak Gilliland",
+      role: "Richard",
+      position: "Noble Assistant",
+      image: "/images/cast/zak-gilliland.jpg",
+    },
+
+    {
+      realName: "Chris Siebert",
+      role: "Harold",
+      position: "Harold",
+      image: "/images/cast/chris-siebert.jpg",
+    },
+    {
+      realName: "Jason Smallen",
+      role: "",
+      position: "",
+      image: "/images/cast/jason-smallen.jpg",
+    },
+    {
+      realName: "Kathryn Smallen",
+      role: "",
+      position: "English Royal Guard",
+      image: "/images/cast/kathryn-smallen.jpg",
+    },
+    {
+      realName: "James Boswell",
+      role: "",
+      position: "French Royal Guard",
+      image: "/images/cast/james-boswell.jpg",
     },
   ],
-  fae: [
-    "The fairies, elves, and mystic beings who bring life to the wilderness",
+  Fae: [
+    "The fairies, elves, and mystic beings who bring life to the wilderness.",
   ],
-  fighter: [
-    "The fighters are the ones who intent to put on a show in the lanes with some form of weaponry",
+  Fighter: [
+    "The fighters are the ones who intent to put on a show in the lanes with some form of weaponry.",
     {
       realName: "Dan Blinn",
       role: "Reynold",
       position: "Hired Mercenary",
-      image: "",
+      image: "/images/cast/dan-blinn.jpg",
     },
     {
       realName: "Noel Kling",
       role: "Griffin Morecotte",
       position: "Kingsman",
-      image: "",
+      image: "/images/cast/noel-kling.jpg",
     },
   ],
-  traveler: [
-    "The traders, craftmaen, entertainers, and their own brand of royalty as they're their own traveling village",
+  Traveler: [
+    "The traders, craftmaen, entertainers, and their own brand of royalty as they're their own traveling village.",
     {
       realName: "David Hernandez",
       role: "Mauritz",
       position: "Hunter/Tradesman",
-      image: "",
+      image: "/images/cast/david-hernandez.jpg",
     },
     {
       realName: "Susanna Ennenbach",
       role: "Sasha",
       position: "Herbalist",
-      image: "",
+      image: "/images/cast/susanna-ennenbach.jpg",
     },
     {
       realName: "Catherine Kuehner",
       role: "Volyana",
       position: "Entertainer",
-      image: "",
+      image: "/images/cast/catherine-kuehner.jpg",
     },
-    { realName: "Grace Mays", role: "Katerina", position: "", image: "" },
+    {
+      realName: "Grace Mays",
+      role: "Katerina",
+      position: "",
+      image: "/images/cast/grace-mays.jpg",
+    },
     {
       realName: "Krista Strahm",
       role: "Zelma",
       position: "Matriarch",
-      image: "",
+      image: "/images/cast/krista-strahm.jpg",
     },
   ],
-  village: [
+  Village: [
     "Those who enjoy their day to day live in Petit Lyon with crafts and simple peace of the festivals.",
   ],
 };
 
 // Those of who we have lost
 const warriors = [
-  "Ashley Lieble",
   "Alex Ryan Harkins",
+  "Ashley Lieble",
   "Bill Cook",
   "Bob Swank",
   "Connie Jacobi",
@@ -135,6 +176,7 @@ const warriors = [
   "Kurt Kuiser",
   "Melvin Due",
   "Michael Hesselback Jr",
+  "Owain Phyfe",
   "Red Pruitt",
   "Renee Ewart",
   "Sandy Goodall",
--- javascript/header.js
+++ javascript/header.js
@@ -9,7 +9,7 @@ function response() {
     resNav.className = "nav-bar";
   }
 }
-/*
+
 window.onscroll = function () {
   navScroll();
 };
@@ -25,4 +25,3 @@ function navScroll() {
     navbar.classList.remove("sticky");
   }
 }
-*/
